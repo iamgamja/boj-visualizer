c7a8de337c2d8dcb574097ebc8842a766fce0946
iamgamja <hmmthinking@naver.com>
2023-12-30 12:16:51 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <button className="py-2 px-4 rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+        test
+      </button>
+    </>
+  );
+}
+
+export default App;
